86d53ae2d90010cfe0f438220ca1b5b86eb63482
Bert Spaan <bert.spaan@gmail.com>
2018-02-12 13:22:10 +0100

    more!

--- scripts/extract-masks.sh
+++ scripts/extract-masks.sh
@@ -6,9 +6,11 @@ regex='tiffs/(.*).tiff'
 
 for file in $DIR/../tiffs/*.tiff
 do
-  if [[ $file != *".original."* ]];then
+  if [[ $file != *".original."* ]]; then
     [[ $file =~ $regex ]]
     uuid=${BASH_REMATCH[1]}
-    rio shapes --mask --precision 6 $file | simplify-geojson -t 0.0005 | python -mjson.tool > $DIR/../masks/$uuid.geojson
+    if [ ! -f $DIR/../masks/$uuid.geojson ]; then
+      rio shapes --mask --precision 6 $file | simplify-geojson -t 0.0005 | python -mjson.tool > $DIR/../masks/$uuid.geojson
+    fi
   fi
 done
